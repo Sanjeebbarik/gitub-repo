fb843ea555208360b94ca1e95992f9852a21df92
sanjeeb <sanjeebbarik24@gmail.com>
2022-02-14 17:39:45 +0000

    accepting user name and passward added to login file

--- login.java
+++ login.java
@@ -1,5 +1,8 @@
 // This is the login functionality
 void main ()
 {
-  s.o.p("enter user nsme"
+  s.o.p("enter user nsme"}
+  read user name
+  s.o.p("enter passward")
+  enter passward  
         }
